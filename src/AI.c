c308d9fade66c919da78058bf7b4cbc51fda7443
amin <maghasemzadeh>
2019-01-06 00:02:50 +0330

    AI 1.0

---
+++ src/AI.c
@@ -0,0 +1,5 @@
+//
+// Created by amin on 1/5/19.
+//
+
+#include "AI.h"
---
+++ src/AI.h
@@ -0,0 +1,8 @@
+//
+// Created by amin on 1/5/19.
+//
+
+#ifndef PROJECT_AI_H
+#define PROJECT_AI_H
+
+#endif //PROJECT_AI_H
